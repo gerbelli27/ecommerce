728eecdb9bc73af7b77a98ef491a2de0f5f4d47e
Eduardo Gerbelli <guerbell@hotmail.com>
2022-02-17 23:38:32 +0000

    cart js WIP

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ const plus = document.querySelector('.plus');
 const showQuantity = document.querySelector('.quantity');
 const addCart = document.querySelector('.add-cart');
 
+/* Side Menu Mobile \\\\\\\\\\\ 
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
 
-
-buttonMenu.addEventListener('click', () => { 
+buttonMenu.addEventListener('click', () => {
     menu.classList.toggle('menu__list--active')
 })
 
@@ -22,41 +23,51 @@ close.addEventListener('click', () => {
     menu.classList.toggle('menu__list--active')
 })
 
-
+/* Cart functions \\\\\\\\\\\ 
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+var cart =  0;
 var quantity = 0;
-var product = [{ product:"Fall Limited Edition Sneakers",
-                brand:"SNEAKER COMPANY",
-                price:"125.00",
-                quantity: 0
+var product = [{
+    product: "Fall Limited Edition Sneakers",
+    brand: "SNEAKER COMPANY",
+    price: "125.00",
+    quantity: 0
 }]
 
-
+/* Buttons quantity*/
 
 plus.addEventListener('click', () => {
     quantity = quantity + 1;
     showQuantity.innerHTML = quantity;
-    
 })
 
 minus.addEventListener('click', () => {
-    if (quantity > 0){
+    if (quantity > 0) {
         quantity = quantity - 1;
         showQuantity.innerHTML = quantity;
-    console.log(quantity);
     } else {
-        window.alert('Plesase add quantity')
+        window.alert('Please add quantity')
     }
 })
 
 addCart.addEventListener('click', () => {
-    
     product[0].quantity = quantity;
-console.log(product);
+    cart = product[0].quantity
     quantity = 0;
     showQuantity.innerHTML = quantity;
 })
 
- 
+/* Show Cart \\\\\\\\\\\ 
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
 
+const cartButton = document.querySelector('.cart');
 
+cartButton.addEventListener('click', () => {
+   if(cart > 0){
+       
+    console.log(product[0].product + "$" + product[0].price + "X" +  cart  + " " + (cart * product[0].price));
+   }else{
+       window.alert('Your cart is empty !')
+   }
+})
 
